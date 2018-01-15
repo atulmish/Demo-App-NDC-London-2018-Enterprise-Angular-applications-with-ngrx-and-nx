7badf0873091b151762698f333a31c4f2ee563ca
Duncan Hunter <duncan@duncanhunter.com.au>
2018-01-15 16:50:51 +0000

    at ngrx

--- libs/auth/src/containers/login/login.component.ts
+++ libs/auth/src/containers/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Authenticate } from '@demo-app/data-models';
+import { Authenticate, User } from '@demo-app/data-models';
 import { AuthService } from './../../services/auth.service';
 import { catchError, map } from 'rxjs/operators';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'login',
@@ -12,11 +13,15 @@ import { of } from 'rxjs/observable/of';
 })
 export class LoginComponent implements OnInit {
   user$: Observable<any>;
-  constructor(private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private authService: AuthService) {}
 
   ngOnInit() {}
 
   login(authenticate: Authenticate): void {
-    this.authService.login(authenticate).subscribe();
+    this.authService.login(authenticate).subscribe(
+      (user: User) => this.router.navigate([`/user-profile/${user.id}`])
+    );
   }
 }
